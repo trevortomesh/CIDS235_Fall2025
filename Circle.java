87116c639b8db14f8a8141b51abf5e2c2b45a212
Trevor Tomesh <slugzzz@gmail.com>
2025-09-05 10:37:34 -0500

    Add Circle class and main method to demonstrate usage

---
+++ Circle.java
@@ -0,0 +1,10 @@
+public class Circle {
+    double radius = 1;
+
+    Circle(){
+    }
+
+    Circle(double newRadius){
+        radius = newRadius;
+    }
+}
---
+++ Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        // System.out.println("Hello, World!");
+        // String niko_name = "Niko";
+        // char niko_sex = 'M';
+        // String niko_species = "cat";
+        // int niko_age = 5;
+        // String niko_color = "black";
+
+        // String brandy_name = "Brandy";
+        // char brandy_sex = 'F';
+        // String brandy_species = "dog";
+        // int brandy_age = 6;
+        // String brandy_color = "tan";
+
+        // System.out.println("Niko's Information:");
+        // System.out.println("Name: " + brandy_name);
+        // System.out.println("Sex: " + niko_sex);
+        // System.out.println("Species: " + niko_species);
+        // System.out.println("Age: " + niko_age);
+        // System.out.println("Color: " + niko_color);
+
+        Circle circle1 = new Circle(5);
+        System.out.println(circle1.radius);
+    }
+}
