7d9ccde5d1cc0bc2912921ecda4c51a1825a2cee
Trevor Tomesh <slugzzz@gmail.com>
2025-09-08 09:50:11 -0500

    Refactor Circle and Main classes to improve constructor output and demonstrate area calculation

--- Circle.java
+++ Circle.java
@@ -2,9 +2,16 @@ public class Circle {
     double radius = 1;
 
     Circle(){
+        System.out.println("This is the default constructor");
+        System.out.println("No argument given -- the radius is: " + radius);
     }
 
     Circle(double newRadius){
+        System.out.println("This is the parameterized constructor");
         radius = newRadius;
+    }
+
+    double getArea(){
+        return Math.PI * radius * radius;
     }
 }
--- Main.java
+++ Main.java
@@ -1,26 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        // System.out.println("Hello, World!");
-        // String niko_name = "Niko";
-        // char niko_sex = 'M';
-        // String niko_species = "cat";
-        // int niko_age = 5;
-        // String niko_color = "black";
 
-        // String brandy_name = "Brandy";
-        // char brandy_sex = 'F';
-        // String brandy_species = "dog";
-        // int brandy_age = 6;
-        // String brandy_color = "tan";
-
-        // System.out.println("Niko's Information:");
-        // System.out.println("Name: " + brandy_name);
-        // System.out.println("Sex: " + niko_sex);
-        // System.out.println("Species: " + niko_species);
-        // System.out.println("Age: " + niko_age);
-        // System.out.println("Color: " + niko_color);
-
-        Circle circle1 = new Circle(5);
-        System.out.println(circle1.radius);
+        Circle circle1 = new Circle();
+        Circle circle2 = new Circle(5);
+        System.out.println("Circle1 has a radius of: " + circle1.radius);
+        System.out.println("Circle2 has a radius of: "+ circle2.radius);
+       // System.out.println("The area of circle2 is: " + (Math.PI * circle2.radius * circle2.radius));
+        System.out.println("The area of circle2 is: " + circle2.getArea());
     }
 }
